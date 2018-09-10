c3a7e69a777bac575229a4a3056fe5392a17cb8f
Elijah Legler <legler.elijah@gmail.com>
2018-09-09 22:11:02 -0400

    final commit
    completely debugged code

---
+++ Debug-Exercise-master/webconfig.php
@@ -0,0 +1,6 @@
+<?php
+//added ABSOLUTE_PATH and URL_Root
+define('ABSOLUTE_PATH', '/home/elegler/htdocs/CIT313/SP2018/a1/Debug-Exercise-master');
+define('URL_ROOT', 'http://corsair.cs.iupui.edu:23661/CIT313/SP2018/a1/Debug-Exercise-master');
+
+?>
